5ee4e3d157c676ef76fc2331d47db024ae127754
almaz27 <zigyiass@gmail.com>
2024-05-17 11:00:12 +0300

    Trying to send rows to tcpdf action

--- views/nomadex/outbound-order-items/group.php
+++ views/nomadex/outbound-order-items/group.php
@@ -2,6 +2,7 @@
 
 use yii\helpers\Html;
 use yii\widgets\DetailView;
+use yii\bootstrap5\Button;
 
 /** @var yii\web\View $this */
 /** @var app\models\nomadex\Stock $model */
@@ -12,8 +13,14 @@ $this->params['breadcrumbs'][] = $this->title;
 \yii\web\YiiAsset::register($this);
 ?>
 <div class="stock-group" id="test">
-    <table class="table table-striped table-bordered">
     
+    <div class="d-flex flex-row-reverse">
+    <!-- Html::a('PDF', ['/outbound-order-items/action'], ['class'=>'btn btn-primary'])  -->
+    <?= Html::a('<span class="btn-label">Convert to PDF </span>', ['topdf', 'rows' => $rows], ['class' => 'btn btn-primary']) ?>
+</div>
+
+</div>
+    <table class="table table-striped table-bordered">
     <tr>
         <th><?= Html::encode($rows[0]->attributeLabels()['product_name'])?></th>
         <th><?= Html::encode($rows[0]->attributeLabels()['status'])?></th>
